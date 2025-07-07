37d6e990923e93ee664e18ee5369080683de4618
LvMaxZz1 <2253862576@qq.com>
2025-07-07 21:43:38 +0800

    feat(webchatui): 实现基本聊天界面和功能
    - 移除原有的 logo 和导航栏
    - 添加聊天窗口和输入框
    - 实现消息发送和显示逻辑- 添加系统消息初始化功能
    - 优化页面样式，包括聊天项样式和布局

--- WebChatUi/webchatui/src/views/HomeView.vue
+++ WebChatUi/webchatui/src/views/HomeView.vue
@@ -1,9 +1,84 @@
+<template>
+  <div>
+    <div class="chat-pad">
+      <div class="chat-item" v-for="chat in chats">
+        <div v-if="chat.role == 'user'" style="overflow: hidden">
+          <p class="user-chat">{{ chat.content }}</p>
+        </div>
+        <div v-else-if="chat.role == 'assistant'" style="overflow: hidden">
+          <p class="assistant-chat">
+            {{ chat.content }}
+          </p>
+        </div>
+      </div>
+      <textarea v-model="input" name="" id="" rows="6"></textarea>
+      <button @click="onSend" :disabled="!canClickSend">发送</button>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { computed, onMounted, ref } from 'vue'
+
+const input = ref('')
+const canSend = ref(true)
+
+const canClickSend = computed(() => input.value.trim() !== '' && canSend.value)
+
+const chats = ref<any>([])
+
+function onSend() {
+  canSend.value = false
+  let baseUrl = 'http://localhost:5262/'
+  let url = baseUrl + 'api/ai'
+  chats.value.push({
+    role: 'user',
+    content: input.value,
+  })
+  input.value = ''
+  const response = fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ ChatItemDtos: chats.value }),
+  })
+  canSend.value = true
+}
+
+onMounted(() => {
+  chats.value.push({
+    role: 'system',
+    content: '你是一个诗人',
+  })
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+.chat-pad {
+  width: 600px;
+  margin: auto;
+}
+
+.user-chat {
+  background-color: skyblue;
+  float: right;
+  color: black;
+  border-radius: 4xp;
+  padding: 10px;
+}
+
+.assistant-chat {
+  background-color: #ccc;
+  float: left;
+  color: black;
+  border-radius: 4xp;
+  padding: 10px;
+}
+
+textarea {
+  width: 100%;
+  resize: none;
+  margin-top: 20px;
+}
+</style>
